280099b68cb1b751c577fc21b8a9948ce2ffa9f8
lehair <162772799+lehair@users.noreply.github.com>
2025-12-19 22:11:46 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -20,12 +20,11 @@ except FileNotFoundError:
 # ==========================================
 print("Dang tien xu ly...")
 
-# Chọn các đặc trưng (Features) số quan trọng
+
 features = ['Area', 'Floors', 'Bedrooms', 'Bathrooms', 'Frontage', 'Access Road']
 target = 'Price'
 
-# Xử lý giá trị thiếu (Missing Values)
-# Với dữ liệu số, ta điền bằng giá trị trung vị (median) để tránh sai lệch do nhiễu
+
 for col in features:
     if col in df.columns:
         df[col] = df[col].fillna(df[col].median())
@@ -36,8 +35,7 @@ df = df.dropna(subset=[target])
 # ==========================================
 # 3. XỬ LÝ DỮ LIỆU (Feature Engineering)
 # ==========================================
-# (Ở bài toán đơn giản này, bước 2 và 3 thường gộp chung. 
-# Nếu dữ liệu phức tạp hơn, bước này sẽ bao gồm: chuẩn hóa dữ liệu, one-hot encoding...)
+
 
 X = df[features]
 y = df[target]
@@ -68,4 +66,5 @@ print("-" * 30)
 
 # Lưu mô hình đã train ra file .pkl để App có thể dùng
 joblib.dump(model, 'house_model.pkl')
+
 print("Da luu mo hinh thanh cong vao file 'house_model.pkl'")
